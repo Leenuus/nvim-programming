da647604977086526af220d676a51f9ff5455869
Leenuus <linusrach24@gmail.com>
2024-04-17 22:27:24 +0800

    two ways to iterate treesitter query results

--- lua/play/play-with-treesitter.lua
+++ lua/play/play-with-treesitter.lua
@@ -9,15 +9,23 @@ local root = tree:root()
 
 local t = vim.treesitter.get_node_text(root, 0)
 
-local query_text = [[(variable_declaration) @good]]
+local query_text = [[(variable_declaration (assignment_statement) @a) @d]]
 local q = vim.treesitter.query.parse(lang, query_text)
 
-for id, node, metadata in q:iter_captures(root, 0, 0, 22) do
-	local name = q.captures[id] -- name of the capture in the query
-	-- typically useful info about the node:
+local start_line = 0
+local end_line = 35
+for id, node, metadata in q:iter_captures(root, 0, start_line, end_line) do
+	local name = q.captures[id]
 	local type = node:type()
-	logger.info(vim.treesitter.get_node_text(node, 0))
-	local row1, col1, row2, col2 = node:range() -- range of the capture
-	logger.info(name)
-	logger.info(type)
+	logger.info("node: ", vim.treesitter.get_node_text(node, 0))
+	logger.info("name:", name)
+	logger.info("type: ", type)
+end
+
+for _, match, _ in q:iter_matches(tree:root(), 0, start_line, end_line) do
+	for id, node in pairs(match) do
+		local name = q.captures[id]
+		logger.info("name:", name)
+		logger.info("node: ", vim.treesitter.get_node_text(node, 0))
+	end
 end
